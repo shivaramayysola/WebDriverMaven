580bc42d61d2aaadf8b7ab10c461c40a3b179d3c
shivaramayysola <shivaramayysola@gmail.com>
2019-03-23 20:07:11 +0530

    Initial Commit

---
+++ src/test/java/com/w2a/WebDriverMaven/LoginTest.java
@@ -0,0 +1,39 @@
+package com.w2a.WebDriverMaven;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.firefox.*;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+
+ public class LoginTest {
+	
+	public static  WebDriver driver;
+	
+	@BeforeSuite
+	public void setUp() {
+		
+		driver = new FirefoxDriver();
+		
+		
+		
+	}
+	@Test
+	public void doLogin() {
+		
+		driver.get("http://gmail.com");
+		driver.findElement(By.id("Email")).sendKeys("shivaram933@gmail.com");
+		driver.findElement(By.id("passwrd")).sendKeys("sdfs");
+	}
+	
+	@AfterSuite
+	public void tearDown() {
+		
+		driver.quit();
+	}
+}
+
+
